b82a07f6d76c6b970250b6f5b6df4a6f0050d5ca
kali-maha <hermetichedonism@gmail.com>
2021-06-04 16:42:26 +0200

    merge upstream

--- react-delegationdashboard/src/components/Overview/Cards/index.tsx
+++ react-delegationdashboard/src/components/Overview/Cards/index.tsx
@@ -58,7 +58,7 @@ const Views = () => {
     () => {
       getNetworkStake();
     },
-    /* eslint-disable react-hooks/exhaustive-deps */ []
+    /* eslint-disable react-hooks/exhaustive-deps */[]
   );
 
   return (
